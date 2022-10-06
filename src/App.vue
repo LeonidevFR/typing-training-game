200305187768ae8b614d8e2249098b7a4444b1b7
LeonidevFR <leonardploton.dev@gmail.com>
2022-10-06 23:26:26 +0200

    Init project + Add Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TypingTextInput from "./components/TypingTextInput.vue";
+</script>
+
+<template>
+  <TypingTextInput />
+</template>
+
+<style scoped></style>
---
+++ src/components/TypingTextInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    type="text"
+    placeholder="The quick brown fox plays in the meadow"
+    v-model="userInput"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const userInput = ref("");
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
