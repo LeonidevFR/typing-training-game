869c41c868718050dd66d13df174158e5c8e0906
LeonidevFR <leonardploton.dev@gmail.com>
2022-12-17 11:11:05 +0100

    [WIP] Typing  input & comparison

--- src/components/TypingTextInput.vue
+++ src/components/TypingTextInput.vue
@@ -1,19 +1,58 @@
 <template>
-  <input
-    type="text"
-    placeholder="The quick brown fox plays in the meadow"
-    v-model="userInput"
-  />
+  <div class="relative flex justify-start">
+    <input
+      id="invisible-input"
+      type="text"
+      class="w-full h-20 rounded-lg text-4xl font-medium pl-4"
+      v-model="inputCharacter"
+      autofocus
+      @input="handleInput()"
+    />
+    <div class="w-auto text-4xl font-medium pl-4 absolute top-4">
+      <span
+        v-for="(item, index) in exercisePhrase.replaceAll(' ', '`').split('')"
+        :key="index"
+        :class="charIndex > index ? 'text-teal-500' : ''"
+      >
+        {{ item === "`" ? "&nbsp" : item }}
+      </span>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
+const exercisePhrase = ref("The quick brown fox plays in the meadow");
 const userInput = ref("");
+const inputCharacter = ref("");
+const lastInputCharacter = ref("");
+const charIndex = ref(0);
+
+watch(userInput, (oldVal, newVal) => {
+  if (oldVal !== newVal) {
+    let newValLength = newVal.value.length();
+    let newchar = newVal.value[newValLength];
+    if (newchar === exercisePhrase.value[newValLength]) {
+      console.log("FELICITATIONS");
+      charIndex.value++;
+    }
+    inputCharacter.value = "";
+  }
+});
+
+const handleInput = () => {
+  if (inputCharacter.value === exercisePhrase.value[charIndex.value]) {
+    console.log("FELICITATIONS");
+    charIndex.value++;
+  }
+  lastInputCharacter.value += inputCharacter.value;
+  inputCharacter.value = "";
+};
 </script>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+#invisible-input {
+  /* color: transparent; */
 }
 </style>
